0cab7740b5dea33786db466f319db360d0fc3b5d
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-10 22:58:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,14 +6,14 @@ def speak_to_grandma(string)
   elsif string == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
     
-  elsif string == "Hi"
+  elsif string == "Hi!"
     return "HUH?! SPEAK UP SONNY!"
     
   elsif string == 'WHAT DID YOU EAT TODAY?'
     return "NO, NOT SINCE 1938!"
     
   elsif string == "WHAT?"
-    return "NO, NOT SINE 1938!"
+    return "NO, NOT SINCE 1938!"
   end
     
 end
