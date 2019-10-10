d63f612ec3a7a2e377d7a4ff4d32bff8147c7d41
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-10 22:42:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,7 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma("Hi, Nana how are you?")
+  return "HUH?! SPEAK UP, SONNY!"
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
