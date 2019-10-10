fe0ad9d3316db538fc0f088eb5e1dae8cbed9c10
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-10 22:59:21 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,22 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(string)
+  if string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  
+  elsif string == "Hi Nana, how are you?"
+    return "HUH?! SPEAK UP, SONNY!"
+    
+  elsif string == "Hi!"
+    return "HUH?! SPEAK UP, SONNY!"
+    
+  elsif string == 'WHAT DID YOU EAT TODAY?'
+    return "NO, NOT SINCE 1938!"
+    
+  elsif string == "WHAT?"
+    return "NO, NOT SINCE 1938!"
+  end
+    
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
